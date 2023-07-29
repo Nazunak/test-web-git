5135c3a1168f04e7869a8d038c7178ae4c8f1db3
pavlo <work.reiste@gmail.com>
2023-07-29 12:55:07 +0300

    test commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss';
+import mylogo from './assets/flaticon-code-lapetop.png';
+
+const mainImage = document.getElementById("mainImage")
+mainImage.src = mylogo
+
+const hello = require('./js/hello');
