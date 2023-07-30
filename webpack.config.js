fa380cbf14e3c5afda28bab49066b0fb90c9273c
pavlo <work.reiste@gmail.com>
2023-07-30 14:35:03 +0300

    assets

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
-        assetModuleFilename: '[name][ext]',
+        assetModuleFilename: 'assets/[hash][ext][query]',
         clean: true
     },
     performance: {
